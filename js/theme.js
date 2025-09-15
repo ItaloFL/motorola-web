7d31efb7219e621ee3d27783dbbbc70a259a857f
ItaloFL <grd788@gmail.com>
2025-09-15 17:36:39 -0500

    feat :sparkles: theme

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,15 @@
 const btnTheme = document.getElementById("btn-theme");
+const html = document.documentElement;
+
+const theme = localStorage.getItem("motorola-theme")
+
+if (theme) {
+  html.setAttribute("data-theme", theme)
+}
 
 btnTheme.addEventListener("click", () => {
-  const html = document.documentElement;
   const current = html.getAttribute("data-theme");
-  html.setAttribute("data-theme", current === "light" ? "dark" : "light");
+  const newTheme = current === "light" ? "dark" : "light"
+  html.setAttribute("data-theme", newTheme);
+  localStorage.setItem("motorola-theme", newTheme)
 });
